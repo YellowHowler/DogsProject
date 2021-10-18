60aa87e95b5636d79ba469ea2ecad19e800c2515
Asti477 <rittachoi1@gmail.com>
2021-10-18 20:14:06 +0900

    added game

---
+++ AbandonedDogs/Assets/scripts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DogBreed
+{
+    BichonFrise, Shiba, Poodle, GoldenRetriever, Husky, Pomeranian, Beagle, Dachshund, Maltese
+}
+
+public class GameManager : MonoBehaviour
+{
+    //item 관련 정보
+    //money, diamond
+    //dog 입양보낸 얘
+
+    public static GameManager instance;
+
+    public List<int> itemOccupiedList = new List<int>();
+
+    private int itemCount = 50;
+    private int money;
+    private int diamond;
+    private int dog;
+
+    public DogBreed currentBreed = DogBreed.Husky;
+
+    public void SaveItem()
+    {
+        for (int i = 0; i < itemOccupiedList.Count; i++)
+        {
+            PlayerPrefs.SetInt("Item" + i, itemOccupiedList[i]);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void SaveAsset()
+    {
+        PlayerPrefs.SetInt("Money", money);
+        PlayerPrefs.SetInt("Diamond", diamond);
+        PlayerPrefs.SetInt("Dog", dog);
+    }
+
+    public void LoadItem()
+    {
+        itemOccupiedList.Clear();
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            if (PlayerPrefs.HasKey("item" + i))
+            {
+                itemOccupiedList.Add(PlayerPrefs.GetInt("Item" + i));
+            }
+
+            else
+            {
+                itemOccupiedList.Add(0);
+            }
+        }
+    }
+
+    public void LoadAsset()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+        {
+            money = PlayerPrefs.GetInt("Money");
+        }
+
+        if (PlayerPrefs.HasKey("Diamond"))
+        {
+            diamond = PlayerPrefs.GetInt("Diamond");
+        }
+
+        if (PlayerPrefs.HasKey("Dog"))
+        {
+            dog = PlayerPrefs.GetInt("Dog");
+        }
+
+    }
+
+    void Initialize()
+    {
+        LoadItem();
+        LoadAsset();
+    }
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        Initialize();
+    }
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SaveItem();
+            SaveAsset();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Initialize();
+        }
+    }
+}
---
+++ AbandonedDogs/Assets/scripts/player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 0;
+    private int defense = 0;
+    private float moveSpeed = 0f;
+
+    private bool noticeObstacle = false;
+
+    public DogBreed currentBreed = DogBreed.Husky;
+
+    private void Start()
+    {
+        switch(GameManager.instance.currentBreed)
+        {
+            case DogBreed.Beagle:
+                break;
+
+            case DogBreed.BichonFrise:
+                break;
+
+            case DogBreed.Dachshund:
+                break;
+
+            case DogBreed.GoldenRetriever:
+                break;
+
+            case DogBreed.Husky:
+                break;
+
+            case DogBreed.Maltese:
+                break;
+
+            case DogBreed.Pomeranian:
+                break;
+
+            case DogBreed.Poodle:
+                break;
+
+            case DogBreed.Shiba:
+                break;
+        }
+    }
+
+    void initializeDog(int hp, float moveSpeed, int defense, bool noticeObstacle)
+    {
+        this.hp = hp;
+        this.moveSpeed = moveSpeed;
+        this.defense = defense;
+        this.noticeObstacle = noticeObstacle;
+    }
+}
